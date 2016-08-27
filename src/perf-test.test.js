78c6fee9dd7bfbbee5356f5959e4c975794c3e19
dchambers <dominic.chambers@gmail.com>
2016-08-27 14:26:01 +0100

    Use larger tree for testing
    We're now using a 3x3 tree instead of a 2x2 tree to give additional
    confidence that the 'create' and 'mutate' functions acutally work.

--- src/perf-test.test.js
+++ src/perf-test.test.js
@@ -4,12 +4,19 @@ import map from './immutable-map';
 
 const expectedObj = {
   'item #1': {
-    'item #1': {},
-    'item #2': {}
+    'item #1': {'item #1': {}, 'item #2': {}, 'item #3': {}},
+    'item #2': {'item #1': {}, 'item #2': {}, 'item #3': {}},
+    'item #3': {'item #1': {}, 'item #2': {}, 'item #3': {}}
   },
   'item #2': {
-    'item #1': {},
-    'item #2': {}
+    'item #1': {'item #1': {}, 'item #2': {}, 'item #3': {}},
+    'item #2': {'item #1': {}, 'item #2': {}, 'item #3': {}},
+    'item #3': {'item #1': {}, 'item #2': {}, 'item #3': {}}
+  },
+  'item #3': {
+    'item #1': {'item #1': {}, 'item #2': {}, 'item #3': {}},
+    'item #2': {'item #1': {}, 'item #2': {}, 'item #3': {}},
+    'item #3': {'item #1': {}, 'item #2': {}, 'item #3': {}}
   }
 };
 
@@ -17,45 +24,54 @@ const expectedMutatedObj = {
   'item #1': '...',
   'item #2': {
     'item #1': '...',
-    'item #2': {}
-  }
+    'item #2': {'item #1': '...', 'item #2': {}, 'item #3': '...'},
+    'item #3': '...'
+  },
+  'item #3': '...'
 };
 
 const expectedPartiallyMutatedObj = {
   'item #1': {
     'item #1': '...',
-    'item #2': {}
+    'item #2': {'item #1': '...', 'item #2': {}, 'item #3': '...'},
+    'item #3': '...'
   },
   'item #2': {
     'item #1': '...',
-    'item #2': {}
+    'item #2': {'item #1': '...', 'item #2': {}, 'item #3': '...'},
+    'item #3': '...'
+  },
+  'item #3': {
+    'item #1': '...',
+    'item #2': {'item #1': '...', 'item #2': {}, 'item #3': '...'},
+    'item #3': '...'
   }
 };
 
 test('obj.create works as expected', t => {
-  t.deepEqual(obj.create(2, 2), expectedObj);
+  t.deepEqual(obj.create(3, 3), expectedObj);
 });
 
 test('obj.mutate works as expected', t => {
-  t.deepEqual(obj.mutate(obj.create(2, 2)), expectedMutatedObj);
-  t.deepEqual(obj.mutate(obj.create(2, 2), 1), expectedPartiallyMutatedObj);
+  t.deepEqual(obj.mutate(obj.create(3, 3)), expectedMutatedObj);
+  t.deepEqual(obj.mutate(obj.create(3, 3), 1), expectedPartiallyMutatedObj);
 });
 
 test('map.create works as expected', t => {
-  t.deepEqual(map.create(2, 2).toJS(), expectedObj);
+  t.deepEqual(map.create(3, 3).toJS(), expectedObj);
 });
 
 test('map.mutate works as expected', t => {
-  t.deepEqual(map.mutate(map.create(2, 2)).toJS(), expectedMutatedObj);
-  t.deepEqual(map.mutate(map.create(2, 2), 1).toJS(), expectedPartiallyMutatedObj);
+  t.deepEqual(map.mutate(map.create(3, 3)).toJS(), expectedMutatedObj);
+  t.deepEqual(map.mutate(map.create(3, 3), 1).toJS(), expectedPartiallyMutatedObj);
 });
 
 test('map.optimizedMutate works as expected', t => {
-  t.deepEqual(map.optimizedMutate(map.create(2, 2)).toJS(), expectedMutatedObj);
-  t.deepEqual(map.optimizedMutate(map.create(2, 2), 1).toJS(), expectedPartiallyMutatedObj);
+  t.deepEqual(map.optimizedMutate(map.create(3, 3)).toJS(), expectedMutatedObj);
+  t.deepEqual(map.optimizedMutate(map.create(3, 3), 1).toJS(), expectedPartiallyMutatedObj);
 });
 
 test('map.globalMutate works as expected', t => {
-  t.deepEqual(map.globalMutate(map.create(2, 2)).toJS(), expectedMutatedObj);
-  t.deepEqual(map.globalMutate(map.create(2, 2), 1).toJS(), expectedPartiallyMutatedObj);
+  t.deepEqual(map.globalMutate(map.create(3, 3)).toJS(), expectedMutatedObj);
+  t.deepEqual(map.globalMutate(map.create(3, 3), 1).toJS(), expectedPartiallyMutatedObj);
 });
